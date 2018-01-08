56b0fba826032c8344327a0537f8d5281a1e3564
Manuel Pichardo <mlpichardo1@gmail.com>
2018-01-08 22:38:20 +0000

    Completed lesson 5

--- node.js
+++ node.js
@@ -151,3 +151,56 @@ app.get('/tweets', function(req, res) {
  res.sendFile(__dirname + "/tweets.html"); 
 });
 app.listen(8080);
+//CONT
+var express = require('express');
+var app = express();
+var quotes = {
+  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+  'berners-lee': 'The Web does not just connect machines, it connects people',
+  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+};
+
+app.get('/quotes/:name', function(req, res) {
+  var quote = quotes[req.params.name];
+  res.render('quote.ejs', {
+    name: req.params.name,
+    quote: quote
+  });
+
+app.listen(8080);
+//URL Building
+var url = require('url');
+
+var options = {
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: { q: "codeschool"}
+};
+
+var searchURL = url.format(options);
+var request = require('request');
+request(searchURL, function(err, response, body) {
+console.log(body);
+});
+//Server 
+var url = require('url');
+var request = require('request');
+var express = require('express');
+var options = {
+  protocol: "http:",
+  host: "search.twitter.com",
+  pathname: '/search.json',
+  query: {
+    q: "codeschool"
+  }
+};
+
+var searchURL = url.format(options);
+
+var app = express();
+app.get('/', function(req, res) {
+request(searchURL).pipe(res);
+  }); // Create server here
+app.listen(8080);
