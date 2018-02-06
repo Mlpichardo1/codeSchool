644e4664649d7e13eeffa2146b11da61e065fe26
Manuel Pichardo <mlpichardo1@gmail.com>
2018-02-06 01:55:54 +0000

    adding logic for renting

--- angular/app.js
+++ angular/app.js
@@ -2,15 +2,21 @@
 (function() {
 var app = angular.module('carRental', []);
 
-app.directive('carAvail', function() {
-    return {
-        restrict: 'E',
-        templateURL: 'car-avail.html'
-    };
-});
-
 app.controller('RentalController', function(){
     this.products = cars;
+    this.rental = function(){
+        alert('hola');
+    var n = this.products.length - 1;
+    var i = this.products[n].available;
+   if (i >= 0){
+       i--; 
+       this.products[n].available = i;
+   }
+        console.log(i);
+
+}
+
+
 });
 
 var cars = [
@@ -19,7 +25,7 @@ var cars = [
         image: "https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202017/Magazine-Articles/April/Google-Auto-Profile-images-2017/PRF-496",
         type: 'SUV',
         description: '7 passenger vehicle w/ Navigation and heated seats.',
-        price: 119.95,
+        price: 89.95,
         canPurchase: true,
         available: 3
     },
@@ -28,11 +34,12 @@ var cars = [
         image: "https://www.leithcars.com/assets/stock/colormatched_01/white/640/cc_2018hoc010002_01_640/cc_2018hoc010002_01_640_wa.jpg",
         type: 'Sedan',
         description: '5 passenger vehicle w/ Navigation and 30mpg Highway',
-        price: 99.95,
+        price: 59.95,
         canPurchase: true,
         available: 5
 
     }];
     
+
 })();
 
